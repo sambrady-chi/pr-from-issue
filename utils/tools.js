f94d813ff2390e818ae2398b80936ac8c2aa1812
Prabhat <144340148+prabhat-evertrue@users.noreply.github.com>
2024-09-24 12:13:03 +0530

    increased character length
    increased character length of branch to 30 from 25

--- utils/tools.js
+++ utils/tools.js
@@ -7,7 +7,7 @@ const branchify = (key, summary) => {
 
   return `${lowerKey}-${lowerSummary}`
     .replace(/[^a-z0-9-]/g, "") // removes anything that is not alphanumeric or a hyphen
-    .slice(0, 25) // max length 25 characters for branch, to avoid invalid deployed URL
+    .slice(0, 30) // max length 30 characters for branch, to avoid invalid deployed URL
     .trim(); // remove whitespace
 };
 
